906d68e246b0109a6a0e94036595438dc1bd9f4a
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-04-28 21:06:42 +0000

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    length = len(argv)
+    if length > 2:
+        print("{} arguments:".format(length - 1))
+        for i in range(1, length):
+            print("{}: {}".format(i, argv[i]))
+    elif length == 2:
+        print("{} argument:\n{}: {}".format(1, 1, argv[1]))
+    else:
+        print("0 arguments.")
