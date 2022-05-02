9f81340aee97fecab8e787a26eb02316b7d3d3b5
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-05-02 20:40:50 +0000

    multi returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
     length = len(sentence)
     if length > 0:
         first_char = sentence[0]
-        else:
-            first_char = None
-            tuple = (length, first_char)
-            return tuple
+    else:
+        first_char = None
+        tuple = (length, first_char)
+        return tuple
