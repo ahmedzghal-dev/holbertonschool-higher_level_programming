580be08c778c228bbeadcae1f313b78959e0c74b
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-05-16 10:05:36 +0000

    safe_print_list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for x >= in my_list
+        print('{:d}'.format(x))
+        except ValueError:
+            print("Sorry !")
+            safe_print_list(my_list=[x])
