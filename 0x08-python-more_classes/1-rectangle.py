e5227dcdb273888d8c54fa07ac5097a27bfe0b67
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-05-24 15:12:38 +0000

    rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,42 +1,41 @@
 #!/usr/bin/python3
-"""Rectangle Class
-This is a rectangle
+"""Rectangle module
+This module defines a Rectangle class
 """
 
 
 class Rectangle:
-    """Rectangle Class
-    rectangle with width and height
+    """Rectangle class
+    this is the class for rectangle
     """
+
     def __init__(self, width=0, height=0):
-        """__init__
-        initializing square
+        """__init__ method
+        this method initializes on instance creation
         """
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
-        """defines a rectangle by width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError('width must be a interger')
-        elif value < 0:
-            raise ValueError('width must be >= 0')
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """defines a rectangle by height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError('height must be a interger')
-        elif value < 0:
-            raise ValueError('height must be >= 0')
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
