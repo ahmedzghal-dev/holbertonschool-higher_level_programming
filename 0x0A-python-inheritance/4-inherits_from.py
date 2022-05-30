963e9302ff1df453629eef289c7849a213036ea6
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-05-30 19:01:46 +0000

    inherits_from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -13,4 +13,7 @@ def inherits_from(obj, a_class):
         the specified class - True
         otherwise - False
     """
-    return type(obj) is not a_class
+    if type(obj) != a_class:
+        return True
+    else:
+        return False
