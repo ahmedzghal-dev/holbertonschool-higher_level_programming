1fea289470fd5ae5630057e4ec857321981fbb4d
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-07-06 16:19:56 +0000

    cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,4 +2,4 @@
 --create the table cities (in the database hbtn_0d_usa) on MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(id INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,state_id INT NOT NULL,FOREIGN KEY (state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS cities(id INT UNIQUE NOT NULL  AUTO_INCREMENT PRIMARY KEY,state_id INT NOT NULL,FOREIGN KEY (state_id) REFERENCES states(id), name VARCHAR(256) NOT NULL);
