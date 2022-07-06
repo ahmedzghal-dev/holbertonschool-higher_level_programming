ace7b9c4a2a8171fb234c2b43b3d984176ea2469
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-07-06 13:04:51 +0000

    privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANTS;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
