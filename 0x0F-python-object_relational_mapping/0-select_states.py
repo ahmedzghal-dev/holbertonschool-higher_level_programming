cfc3672f2dfc3017c055c1d2cae916609dff29eb
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-08-05 17:40:15 +0000

    select_states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+        )
+
+cursor = db.cursor()
+cursor.execute("SELECT * from states ORDER BY id ASC")
+result = cursor.fetchall()
+for a in result:
+    print(a)
+cursor.close()
+db.close()
