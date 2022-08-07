cf39214263f18f79863e7aacc54bc71d911b67ca
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-08-07 18:11:34 +0000

    higher level

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Write a script that lists all states from
+the database hbtn_0e_4_usa """
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """ connect to the database"""
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+    )
+
+    cities = []
+    cursor = db.cursor()
+    lists = """SELECT cities.id, cities.name, states.name
+        FROM cities INNER JOIN states ON cities.state_id = states.id
+        WHERE states.name=%s
+        ORDER BY id"""
+    cursor.execute(lists, (argv[4], ))
+    results = cursor.fetchall()
+    for city in results:
+        """ loop through the city"""
+        cities.append(city[1])
+    print(', '.join(cities))
+    cursor.close()
+    """ close the cursor"""
+    db.close()
+    """ close the database"""
