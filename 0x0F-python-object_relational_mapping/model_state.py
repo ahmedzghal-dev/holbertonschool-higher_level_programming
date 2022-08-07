bdcf0b5efa04ce1b8c9d8bd93a8e58ca25cff9cf
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-08-07 20:24:22 +0000

    model_state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Write a python file that contains the class definition of a State"""
 import sqlalchemy
 from sqlalchemy.ext.declaritive import declaritive_base
 from sqlalchemy import Column, Integer, String
@@ -7,7 +8,6 @@ Base = declaritive_base()
 
 Class State(Base):
     """the class state imports Base"""
-
     __tablename__ = 'states'
     id = Column(
         Integer,
