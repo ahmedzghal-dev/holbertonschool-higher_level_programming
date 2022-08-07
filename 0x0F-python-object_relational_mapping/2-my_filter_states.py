605089988d897e5e58cdd4acb81709db159bf003
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-08-07 17:36:36 +0000

    my_filter_states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""  this is a script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """connect to database"""
+    db = MySQLdb.connect(
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+cursor = db.cursor()
+cursor.execute(("SELECT * FROM states".format(argv[4])))
+result = cursor.fetchall()
+for a in result:
+    if a[1] == argv[4]:
+        print(a)
+""" close the cursor"""
+cursor.close()
+""" close the database"""
+db.close()
