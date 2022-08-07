47816ae64fc3263454e999aac5b6bc6d6bcd40b3
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-08-07 17:37:12 +0000

    my_safe_filter_states

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" this is a script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """ connect to the database"""
+    db = MySQLdb.connect(
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+    )
+
+    cursor = db.cursor()
+    naame = "SELECT * FROM states WHERE Name = %s ORDER BY id"
+    cursor.execute(naame, (argv[4], ))
+    result = cursor.fetchall()
+    for i in result:
+        if i[1] == argv[4]:
+            print(i)
+
+    """ close the cursor"""
+    cursor.close()
+    """ close the database"""
+    db.close()
