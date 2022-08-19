2005f23356255d51bdfa40e530ac4c3fd41e3fc2
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-08-19 19:16:36 +0000

    hbtn_header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-Returns a formatted response using urllib
+ Python script that takes in a URL, sends a request to the URL
+ and displays the value of the X-Request-Id variable
+ found in the header of the response
 """
 import urllib.request
 from sys import argv
