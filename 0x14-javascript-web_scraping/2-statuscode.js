47f54b82d6d658548c38193b6cf145204af75c95
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-09-19 02:48:26 +0100

    status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const request = require('request');
+const argv = process.argv;
+request.get(argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
