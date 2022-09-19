a2deb38cb28dbe8bfff0efc113b3008825c471bc
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-09-19 02:48:56 +0100

    js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const argv = process.argv;
+
+request(argv[2], argv[3], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+  fs.writeFile(argv[3], body, 'utf8', function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
