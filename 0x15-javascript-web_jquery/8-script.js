42bd6798b52f4e884db7d8e763d2341284638ca6
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-09-21 23:12:50 +0100

    script 8

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+
+$.get(url, function (movie) {
+  for (let i = 0; i < movie.results.length; i++) {
+    $('ul#list_movies').append(`<li>${movie.results[i].title}</li>`);
+  }
+});
